d8413f9074e9cafeac8f443ed70f2467b0fe4b41
WesleyBruv <wesleyklanga@gmail.com>
2022-09-15 13:34:37 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,22 +11,23 @@
  */
 void print_square(int size)
 {
-        int x;
-        int y;
+	int x, y;
 
-        if (size > 0)
-        {
-                for (x = size; x > 0; x--)
-                {
-                        for (y = size; y > 0; y--)
-                        {
-                                _putchar('#');
-                        }
-                        _putchar('\n');
-                }
-        }
-        else
-        {
-                _putchar('\n');
-        }
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < (size - 1); y++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
