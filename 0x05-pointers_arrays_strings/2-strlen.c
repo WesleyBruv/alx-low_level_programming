1620d8feefdd6a78108de869c2fc514107e07a09
WesleyBruv <wesleyklanga@gmail.com>
2022-09-19 13:30:03 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strelen - will be the length of a string
+ *
+ * @s: input value
+ *
+ * Return: void 
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	{
+		x++;
+	}
+
+	return (x);
+}
