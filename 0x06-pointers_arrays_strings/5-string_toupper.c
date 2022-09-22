04b53874470697522c30058f6ce95a5befc266a1
WesleyBruv <wesleyklanga@gmail.com>
2022-09-22 12:48:36 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_topper - will change all the lower case
+ * letters of a string  to uppercase
+ *
+ * @str: parameter
+ * Return: will return a character
+ */
+
+char *string_toupper(char *str)
+{
+	int array = 0;
+
+	while (str[array++])
+	{
+		if (str[array] >= 'a' && str[array] <= 'z')
+			str[array] -= 32;
+	}
+
+	return (str);
+}
