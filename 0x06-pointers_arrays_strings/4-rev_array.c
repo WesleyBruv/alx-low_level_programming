25795f89b2eac6de851ee4740effda41dc55ff04
WesleyBruv <wesleyklanga@gmail.com>
2022-09-22 12:29:33 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int rev, array;
 
-	for (array = n - 1; array > n / 2; array++)
+	for (array = n - 1; array > n / 2; array--)
 	{
 		rev = a[n - 1 - array];
 		a[n - 1 - array] = a[array];
 		a[array] = rev;
 	}
-}		
+}
